6e5900a601bd2d3009884f6bfafbbdc3f04a93fe
Zach Khong <63769232+multipletwigs@users.noreply.github.com>
2023-04-17 09:52:24 +0800

    Update index.ts

--- nextjs-fe/config/index.ts
+++ nextjs-fe/config/index.ts
@@ -2,4 +2,4 @@ const dev = process.env.NODE_ENV !== "production";
 
 export const baseUrl = dev
   ? "http://localhost:3000"
-  : "https://open-ai-manager.vercel.app";
+  : "https://poemtell.vercel.app";
